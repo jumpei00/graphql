1c435fc59fc46ee46e1c5cc98e63e5c1190bcd9b
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2024-08-24 17:49:24 +0900

    update main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/jumpei00/graphql/backend/internal/graph"
+	"github.com/jumpei00/graphql/backend/internal/graph/middleware"
 	"github.com/jumpei00/graphql/backend/internal/graph/resolver"
 )
 
@@ -22,7 +23,7 @@ func main() {
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolver.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", middleware.AuthenticationMiddleware(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
