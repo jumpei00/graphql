94b13636cb3e76ab11bb824964321d304be0db5d
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2024-08-29 00:36:00 +0900

    update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, gql } from "@apollo/client";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const GET_USERS = gql`
+        query GetUsers {
+            user {
+                id
+                username
+                mailaddress
+                createdAt
+                updatedAt
+            }
+        }
+    `;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const { loading, error, data } = useQuery(GET_USERS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+  return data.locations.map(({ id, username, mailaddress, createdAt, updatedAt }) => (
+    <div key={id}>
+      <p>{`User: ${username}`}</p>
+      <p>{`Mail: ${mailaddress}`}</p>
+      <p>{`Created: ${createdAt}`}</p>
+      <p>{`Updated: ${updatedAt}`}</p>
+    </div>
+  ));
 }
-
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+    uri: "http://localhost:8080",
+    cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
